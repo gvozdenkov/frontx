83993335da56edcc9349fdc7f160bd77ff996932
arty gvozdenkov <gvozdenkov@gmail.com>
2025-04-18 09:21:28 +0300

    refactor(h): extract types to file & remove namespaces

--- packages/runtime/src/h.js
+++ packages/runtime/src/h.js
@@ -1,130 +1,73 @@
 'use strict';
 
-import { pipe } from './utils/pipe';
+import { pipe } from './utils/pipe.js';
 
 /**
- * @namespace HTML
- */
-
-/**
- * Represents all valid HTML tag names
- *
- * @typedef {keyof HTMLElementTagNameMap} HTML.TagName
- */
-
-/**
- * Represents an object containing valid event handlers for an HTML element.
- *
- * @typedef {{ [K in keyof HTMLElementEventMap]?: (event: HTMLElementEventMap[K]) => void }} HTML.EventHandlers
- */
-
-/**
- * Represents valid HTML attributes for a specific HTML tag.
- *
- * @template T
- * @typedef {T extends HTML.TagName ? Partial<HTMLElementTagNameMap[T]> : never} HTML.Attributes
- */
-
-/**
- * Operations for Vdom.
- *
- * @namespace Vdom
- */
-
-/**
- * Valid types of a vNode
- * @typedef {'text' | 'element' | 'fragment'} Vdom.NodeType
- */
-
-/**
- * Defines the structure of a Text vNode
- * @typedef {{ type: Vdom.NodeType, value: string }} Vdom.TextNode
- */
-
-/**
- * @template {HTML.TagName} T
- * @typedef {Object} Vdom.ElementNode
- * @property {Vdom.NodeType} type - vNode type 'text', 'element', 'fragment'
- * @property {T} [tag] - A valid HTML tag name.
- * @property {HTML.Attributes<T> | {}} [props] - An object representing valid HTML attributes
- * @property {HTML.EventHandlers} [on] - Object containing event handlers for the HTML element
- * @property {Vdom.Children<T>[]} [children] - Vdom.Node or string (converted to text node)
- */
-
-/**
- * Defines the structure of a Fragment vNode
- * @template {HTML.TagName} T
- * @typedef {{ type: Vdom.NodeType, children?: Vdom.Children<T>[] }} Vdom.FragmentNode
- */
-
-/**
- * Represents a node in the virtual DOM. It can be a TextNode, ElementNode, or FragmentNode.
- * @template {HTML.TagName} T
- * @typedef {Vdom.TextNode | Vdom.ElementNode<T> | Vdom.FragmentNode<T>} Vdom.Node
- */
-
-/**
- * Represents a type of childeren array.
- * @template {HTML.TagName} T
- * @typedef {string | Vdom.Node<T>} Vdom.Children
+ * @typedef {import('./types.js').HTMLTagName} HTMLTagName
+ * @typedef {import('./types.js').HTMLAttributes} HTMLAttributes
+ * @typedef {import('./types.js').HTMLEventHandlers} HTMLEventHandlers
+ * @typedef {import('./types.js').VNode} VNode
+ * @typedef {import('./types.js').VNodeType} VNodeType
+ * @typedef {import('./types.js').TextVNode} TextVNode
+ * @typedef {import('./types.js').ElementVNode} ElementVNode
+ * @typedef {import('./types.js').FragmentVNode} FragmentVNode
+ * @typedef {import('./types.js').ChildrenVNode} ChildrenVNode
  */
 
 /**
  * Filter nulls form the array
- * @template {HTML.TagName} T
- * @param {Vdom.Children<T>[]} children - Vdom.Node or string (converted to text node)
- * @returns {Vdom.Children<T>[]}
+ * @param {VNode[]} children - VNode or string (converted to text node)
+ * @returns {VNode[]}
  */
 var withoutNulls = (children) => children.filter((child) => child != null);
 
 /**
  * Filter nulls form the array
- * @template {HTML.TagName} T
- * @param {Vdom.Children<T>[]} children - Vdom.Node or string (converted to text node)
- * @returns {Vdom.Node<T>[]}
+ * @param {VNode[]} children - VNode or string (converted to text node)
+ * @returns {VNode[]}
  */
 var mapTextNode = (children) =>
-  // @ts-ignore: return type always Vdom.Node, not a string
+  // @ts-ignore: return type always VNode, not a string
   children.map((child) => (child.constructor === String ? hText(child) : child));
 
 var prepareChildren = pipe(withoutNulls, mapTextNode);
 
 /**
  * Create Element vNode
- * @template {HTML.TagName} T
- * @param {T} tag - HTML tag name
+ * @param {HTMLTagName} tag - HTML tag name
  * @param {Object} [options] - Options for the element.
- * @param {HTML.Attributes<T>} [options.props={}] - HTML element Attributes
- * @param {HTML.EventHandlers} [options.on={}] - Event handlers for the HTML element.
- * @param {Vdom.Children<T>[]} [children=[]] - Array of child nodes, default empty array []
- * @returns {Vdom.ElementNode<T>}
+ * @param {HTMLAttributes} [options.props={}] - HTML element Attributes
+ * @param {HTMLEventHandlers} [options.on={}] - Event handlers for the HTML element.
+ * @param {ChildrenVNode[]} [children=[]] - Array of child nodes, default empty array []
+ * @returns {ElementVNode}
  */
 export var h = (tag, options, children) => ({
   type: 'element',
   tag,
   props: options?.props ?? {},
   on: options?.on ?? {},
-  children: children?.length ? prepareChildren(children) : [],
+  children: (children?.length && prepareChildren(children)) ?? [],
 });
 
 /**
  * Crete Text vNode
  *
  * @param {string} value
- * @returns {Vdom.TextNode}
+ * @returns {TextVNode}
  */
 export var hText = (value) => ({
   type: 'text',
-  value,
+  value: value ?? '',
+  el: undefined,
 });
 
 /**
  * Create Fragment vNode
- * @template {HTML.TagName} T
- * @param {Vdom.Children<T>[]} vNodes
- * @returns {Vdom.FragmentNode<T>}
+ * @param {ChildrenVNode[]} children - array of VNode or/and string
+ * @returns {FragmentVNode}
  */
-export var hFrag = (vNodes) => ({
+export var hFrag = (children) => ({
   type: 'fragment',
-  children: vNodes?.length ? mapTextNode(withoutNulls(vNodes)) : [],
+  children: (children?.length && prepareChildren(children)) ?? [],
+  el: undefined,
 });
---
+++ packages/runtime/src/types.js
@@ -0,0 +1,60 @@
+/**
+ * Represents all valid HTML tag names
+ *
+ * @typedef {keyof HTMLElementTagNameMap} HTMLTagName
+ */
+
+/**
+ * Represents an object containing valid event handlers for an HTML element.
+ *
+ * @typedef {{ [K in keyof HTMLElementEventMap]?: (event: HTMLElementEventMap[K]) => void }} HTMLEventHandlers
+ */
+
+/**
+ * Represents valid HTML attributes for a specific HTML tag.
+ *
+ * @typedef {Partial<HTMLElementTagNameMap[HTMLTagName]>} HTMLAttributes
+ */
+
+/**
+ * Valid types of a vNode
+ * @typedef {'text' | 'element' | 'fragment'} VNodeType
+ */
+
+/**
+ * Represents a node in the virtual DOM. It can be a TextNode, ElementNode, or FragmentNode.
+ * @typedef {TextVNode | ElementVNode | FragmentVNode} VNode
+ */
+
+/**
+ * Defines the structure of a Text vNode
+ * @typedef {Object} TextVNode
+ * @property {'text'} type - The type of the virtual node = 'text'.
+ * @property {string} value - The text of the text node.
+ * @property {Text} [el] - The mounted element.
+ */
+
+/**
+ * @typedef {Object} ElementVNode
+ * @property {'element'} type - The type of the virtual node = 'element'.
+ * @property {HTMLTagName} tag - A valid HTML tag name.
+ * @property {HTMLAttributes | {}} [props] - An object representing valid HTML attributes
+ * @property {HTMLEventHandlers} [on] - Object containing event handlers for the HTML element
+ * @property {VNode[]} children - VNode or string (converted to text node)
+ * @property {HTMLElement} [el] - The mounted element
+ */
+
+/**
+ * Defines the structure of a Fragment vNode
+ * @typedef {Object} FragmentVNode
+ * @property {'fragment'} type - The type of the virtual node = 'fragment'.
+ * @property {VNode[]} children - The text of the text node.
+ * @property {Text} [el] - The mounted element.
+ */
+
+/**
+ * Represents a type of childeren array.
+ * @typedef {string | VNode} ChildrenVNode
+ */
+
+export {};
